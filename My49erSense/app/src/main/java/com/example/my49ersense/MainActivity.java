1a1b5eddcc1ea03ce18506f94fb66737aab51d7c
VishakhaDixit <vdixit2@uncc.edu>
2022-10-16 22:48:43 -0400

    Add hamburger icon to implement navigation menu.

--- My49erSense/app/src/main/java/com/example/my49ersense/MainActivity.java
+++ My49erSense/app/src/main/java/com/example/my49ersense/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.my49ersense;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +17,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nav_activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         txt = (TextView) findViewById(R.id.homeStatus);
 
