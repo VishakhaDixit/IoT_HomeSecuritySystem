80ea845ae1d4ae3b3aca41217086e0dfa2345153
VishakhaDixit <vdixit2@uncc.edu>
2022-10-16 21:07:06 -0400

    Modify Main app screen to add buttons for changing home status & also add buttons to provide sensor status and control options.

--- My49erSense/app/src/main/java/com/example/my49ersense/MainActivity.java
+++ My49erSense/app/src/main/java/com/example/my49ersense/MainActivity.java
@@ -2,16 +2,44 @@ package com.example.my49ersense;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+    ImageButton btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt = (TextView) findViewById(R.id.homeStatus);
+
+        btn = (ImageButton) findViewById(R.id.btnDisarmed);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setText("Disarmed!");
+            }
+        });
+
+        btn = (ImageButton) findViewById(R.id.btnHomeArm);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setText("Home & Armed!");
+            }
+        });
+
+        btn = (ImageButton) findViewById(R.id.btnAwayArm);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setText("Away & Armed!");
+            }
+        });
     }
-
-    Login login = new Login();
-
-
 }
