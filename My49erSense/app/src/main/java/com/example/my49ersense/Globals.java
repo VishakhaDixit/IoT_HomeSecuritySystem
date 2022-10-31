08cf42b722c45e9756f18e08fef21bef28984190
VishakhaDixit <vdixit2@uncc.edu>
2022-10-31 14:54:57 -0400

    Create Globals file for common IP address & URL used for the app.

---
+++ My49erSense/app/src/main/java/com/example/my49ersense/Globals.java
@@ -0,0 +1,6 @@
+package com.example.my49ersense;
+
+public class Globals {
+    public static final String IP = "10.216.77.168";
+    public static final String PHP_URL = "http://" + IP + "/LoginRegister/";
+}
